e87159f57fd16c981020c71cf67cc05b858ea113
Kanji2105 <jduchimaza6@gmail.com>
2026-09-08 01:41:09 -0400

    finished prototype

--- Assets/Assets/script/Player.cs
+++ Assets/Assets/script/Player.cs
@@ -1,4 +1,7 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
+using static UnityEngine.Rendering.DebugUI.Table;
 
 public class wasd3D : MonoBehaviour
 {
@@ -20,12 +23,14 @@ public class wasd3D : MonoBehaviour
     [Range(0f, 1f)]
     public float airControlFactor = 0.4f;
 
-    //wall ability//jumpoinwall
+    //wall ability//jumpoinwall/climing (test)
 
     bool isOnWall = false;
+    bool isClimbing = false;
     public float wallRunGravity = 2f;
     public float wallJumpForce = 10f;
     public float wallRunSpeed = 150f;
+   
     Vector3 wallNormal;
 
     void Awake()
@@ -74,14 +79,44 @@ public class wasd3D : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space) && isOnWall && !isGrounded)
         {
+            isClimbing = false; // so it dosen't intervene with the wall jump and wall walk 
+            myRB.useGravity = true;
             myRB.linearVelocity = new Vector3(wallNormal.x * wallJumpForce, jumpForce, wallNormal.z * wallJumpForce);
             isOnWall = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.E) && isOnWall)
+        {
+            isClimbing = true;
+            myRB.useGravity = false;     // help to prevent me from falling to fast or drastictly
+
+            //Debug.Log("climbing:" + isClimbing);
+        }
+
+        if (isClimbing)
+        {
+            float climbInput = Input.GetAxis("Vertical");
+
+            myRB.linearVelocity = new Vector3(
+               myRB.linearVelocity.x,
+               climbInput *1.5f,
+               myRB.linearVelocity.z
+            );
+              //W gives positive vertical movement
+              //  S gives negative vertical movement
+              //1.5f is just the climbing speed for now
         }
     }
 
     void FixedUpdate()
     {
         myRB.angularVelocity = Vector3.zero;
+
+
+        if (isClimbing)
+            return;
+        // this will help to make sure it will stick to the wall and it wont intervene with the mocement 
+        // if im climbing, stop executing the normal movement code for this physic frame that hwat retur help here 
         Vector3 inputDir = transform.TransformDirection(Direction());
 
         if (inputDir != Vector3.zero)
@@ -99,14 +134,15 @@ public class wasd3D : MonoBehaviour
         if (isOnWall && !isGrounded)
         {
             myRB.AddForce(Vector3.up * wallRunGravity * Time.fixedDeltaTime);
-            myRB.AddForce(transform.forward * wallRunSpeed * Time.fixedDeltaTime);
+            myRB.AddForce(-transform.forward * wallRunSpeed * Time.fixedDeltaTime);
         }
+
     }
 
     Vector3 Direction()
     {
-        float h = Input.GetAxis("Horizontal"); //horizontal axis is A/D, controls left/right strafe
-        float v = Input.GetAxis("Vertical"); //vertical axis is W/S, controls forwards/backwards walk
+        float h = -Input.GetAxis("Horizontal"); //horizontal axis is A/D, controls left/right strafe
+        float v = -Input.GetAxis("Vertical"); //vertical axis is W/S, controls forwards/backwards walk
         return new Vector3(h, 0, v); //return a direction with horizontal strafe for the X-axis and forwards move for the Z-axis
     }
 
@@ -120,6 +156,16 @@ public class wasd3D : MonoBehaviour
     {
         if (col.gameObject.CompareTag("Ground"))
             isGrounded = false;
+
+        if (col.gameObject.CompareTag("Wall"))
+        {
+            isOnWall = false;
+            isClimbing = false;
+
+            myRB.useGravity = true; // just bring back the gravity on
+        }  // Debug.Log("isOmWall: " + isOnWall);
+
+        
     }
 
     void OnCollisionStay(Collision col)
@@ -128,8 +174,9 @@ public class wasd3D : MonoBehaviour
         {
             isOnWall = true;
             wallNormal = col.contacts[0].normal;
+            
         }
     }
 
-
+    
 }
